24ab28d363c722352a8e757b77059f7f1dd8af5b
Kpritpal12 <kpritpal.12@gmail.com>
2022-03-03 21:01:13 +0530

    ass4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import React from 'react'
+import Navbar from './Component/Navbar'
+import Home from './Component/Home'
+import Student from './Component/Student'
+import Contact from './Component/Contact'
+import './Component/style.css'
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Navbar/>
+    <Routes>
+    <Route path='/' element ={<Home/> }></Route> 
+    <Route path='/home' element ={<Home/> }></Route> 
+    <Route path='/student' element= { <Student/>}  />
+    <Route path='/contact' element= { <Contact/>}  />
+  </Routes>
+    
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Component/Student.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { NavLink } from 'react-router-dom';
+
+
+function createData(name, Age, Course, Batch, change) {
+  return { name, Age, Course, Batch, change };
+}
+
+const rows = [
+  createData('Vaibhav', 26, 'MERN', 'October',<NavLink to='edit'>Edit</NavLink>),
+  createData('Raj', 27, 'MEAN', 'November',<NavLink to='edit'>Edit</NavLink>),
+  createData('Riya', 28, 'MEAN', 'December',<NavLink to='edit'>Edit</NavLink>),
+  createData('Dom', 22, 'MEAN', 'August',<NavLink to='edit'>Edit</NavLink>),
+  createData('Cook', 23, 'MERN', 'October',<NavLink to='edit'>Edit</NavLink>),
+  createData('Hoody', 24, 'MERN', 'January',<NavLink to='edit'>Edit</NavLink>),
+
+];
+
+export default function BasicTable() {
+  return (
+      <div>
+          <h1>STUDENT DETAILS</h1>
+
+       <NavLink to="/student-desc">
+            <button className='add-btn'>Add New Student</button>
+            </NavLink>
+        <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 600 }} aria-label="simple table">
+            <TableHead>
+            <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell align="right">Age</TableCell>
+                <TableCell align="right">Course</TableCell>
+                <TableCell align="right">Batch</TableCell>
+                <TableCell align="right">Change</TableCell>
+            </TableRow>
+            </TableHead>
+            <TableBody>
+            {rows.map((row) => (
+                <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                <TableCell component="th" scope="row">
+                    {row.name}
+                </TableCell>
+                <TableCell align="right">{row.Age}</TableCell>
+                <TableCell align="right">{row.Course}</TableCell>
+                <TableCell align="right">{row.Batch}</TableCell>
+                <TableCell align="right">{row.change}</TableCell>
+                </TableRow>
+            ))}
+            </TableBody>
+        </Table>
+        </TableContainer>
+    </div>
+  );
+}
